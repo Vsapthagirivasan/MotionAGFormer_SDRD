d14e5c0e901b3e910e52064c5e0b09998f2b3d77
Mena S.A. Kamel <37190503+Mena-SA-Kamel@users.noreply.github.com>
2026-04-22 16:35:51 -0400

    Update vis.py
    Fixing issue where last frame inference appears "slower"

--- demo/vis.py
+++ demo/vis.py
@@ -147,8 +147,7 @@ def turn_into_clips(keypoints):
             if clip_length != 243:
                 new_indices = resample(clip_length)
                 clips.append(keypoints_clip[:, new_indices, ...])
-                # downsample = np.unique(new_indices, return_index=True)[1]
-                downsample = np.arange(243)
+                downsample = np.unique(new_indices, return_index=True)[1]
             else:
                 clips.append(keypoints_clip)
     return clips, downsample
